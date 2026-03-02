5cb6e60e1f4adf376e40472b2a61a5da056b7ff4
sadpcd <sadpcd@duck.com>
2026-03-02 06:01:05 +0100

    ➕ Add "Overdue" option to invoice status dropdown

--- src/Views/invoices/create.php
+++ src/Views/invoices/create.php
@@ -307,7 +307,7 @@
                                 <option value="draft">Draft</option>
                                 <option value="sent">Sent</option>
                                 <option value="paid">Paid</option>
-                                <option value="overdue">Overdue</option>
+                                <? if <option value="overdue">Overdue</option>
                             </select>
 
                             <div class="form-check mt-3">
